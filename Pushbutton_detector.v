c340e344aa442b9cb0aaed34ccc7eaca08806b2e
Adham Mohamed <135978982+AdhamMohamed27@users.noreply.github.com>
2024-05-16 00:33:58 +0300

    Add files via upload
    The needed components for the use of buttons BTNC,BTNR,BNTL,BNTU,BNTD

---
+++ Pushbutton_detector.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/15/2024 10:37:11 PM
+// Design Name: 
+// Module Name: Pushbutton_detector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Pushbutton_detector(
+
+    );
+endmodule
---
+++ Rising_edge_detector.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/15/2024 10:38:29 PM
+// Design Name: 
+// Module Name: Rising_edge_detector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Rising_edge_detector(
+
+    );
+endmodule
